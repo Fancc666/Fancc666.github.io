36e61d151bddc88f87b8ff482f02a53968793155
Fancc666 <fcc565455@163.com>
2023-08-17 12:43:03 +0800

    magictower: add github link

--- magictower/reporter.js
+++ magictower/reporter.js
@@ -11,6 +11,10 @@ reporter.report = ()=>{
     let s = document.createElement("script");
     s.src = `http://dev.565455.xyz/report/index.php?tag=${reporter.tag}&inf=${reporter.inf}`;
     s.onload = ()=>s.remove();
+    s.onerror = (e)=>{
+        s.remove();
+        console.log("reporter error:", e, "\n", reporter.tag, reporter.inf);
+    };
     let body = document.getElementsByTagName("body")[0];
     body.appendChild(s);
 }
